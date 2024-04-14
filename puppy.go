3d6783f5e5e3e9ee6e32edeaab817a8a0f43d1ae
dancoe77 <dan@shoeman-tech.com>
2024-04-14 01:25:02 -0700

    removed dependency for dog

--- puppy.go
+++ puppy.go
@@ -1,20 +1,9 @@
 package puppy
 
-import(
-	"github.com/dancoe77/009-dog"
-)
-
 func Bark() string{
 return "Woof!"
 }
 
 func Barks() string {
 return "Woof! Woof! Woof!"
-}
-
-func BigBark() string{
-return dog.WhenGrownUp(Bark())
-
-func BigBarks() string{
-return dog.WhenGrownUp(Barks())
 }
